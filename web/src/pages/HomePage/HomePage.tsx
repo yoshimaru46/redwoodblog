b19accbaa9e441dd532e2d6a3dd0592f2dc3e07a
yoshimaru46 <yshr446@gmail.com>
2022-04-16 17:02:29 +0900

    Change path to home

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -7,9 +7,7 @@ const HomePage = () => {
       <MetaTags title="Home" description="Home page" />
 
       <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
-      </p>
+      <p>Welocome to yoshimaru blog</p>
       <p>
         My default route is named <code>home</code>, link to me with `
         <Link to={routes.home()}>Home</Link>`
