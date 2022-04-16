77bd17b35080d374662d9afcb4f7754e91b1eca3
yoshimaru46 <yshr446@gmail.com>
2022-04-16 21:29:15 +0900

    y rw g cell Articles

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,14 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import ArticlesCell from 'src/components/ArticlesCell'
 
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
 
-      <p>Home</p>
+      <>
+        <ArticlesCell />
+      </>
     </>
   )
 }
